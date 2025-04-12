67e774c9fc3487f70c01621925d64e92125f6953
Akshay Dhumal <akshay.dhumal@nimapinfotech.com>
2025-04-12 09:15:57 +0530

    que 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
 ﻿// 1. WAP to find Second largest element in an array : arr[] = [12, 35, 1, 10, 34, 1, 35], without
 // sorting, without using any built-in methods and without deleting duplicate elements. What will b
 // the time complexity?
+//using System;
+//namespace logicals
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int[] array = { 12, 35, 1, 10, 34, 1, 35 };
+//            int large = 0;
+//            int secLarge = 0;
+//            for (int i = 0; i < array.Length; i++)
+//            {
+//                if (array[i] > large)
+//                {
+//                    large = array[i];
+//                }
+//                if (array[i] < large && array[i] > secLarge)
+//                {
+//                    secLarge = array[i];
+//                }
+//            }
+
+//            Console.WriteLine(secLarge);
+//        }
+//    }
+//}
+
+// 2. WAP to reverse an integer without converting it to a string, without using any built-in methods.
 using System;
 namespace logicals
 {
@@ -8,22 +36,15 @@ namespace logicals
     {
         public static void Main(string[] args)
         {
-            int[] array = { 12, 35, 1, 10, 34, 1, 35 };
-            int large = 0;
-            int secLarge = 0;
-            for (int i = 0; i < array.Length; i++)
+            int n = 123;
+            int reversed = 0;
+            while (n > 0)
             {
-                if (array[i] > large)
-                {
-                    large = array[i];
-                }
-                if (array[i] < large && array[i] > secLarge)
-                {
-                    secLarge = array[i];
-                }
+                int reminder = n % 10;
+                reversed = reminder + reversed * 10;
+                n /= 10;
             }
-
-            Console.WriteLine(secLarge);
+            Console.WriteLine(reversed);
         }
     }
 }
